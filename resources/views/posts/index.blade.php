1c644ad6459295ec4e7d20b2b157429c5bbb8739
Giorgian Robescu <robescu.george96@gmail.com>
2019-03-27 18:51:39 +0200

    Daca mai multe cheltuieli sunt in aceeasi zi, apar in acelasi card

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,17 +3,37 @@
 @section('content')
     <h1>Cheltuieli</h1>
     @if(count($posts) > 0)
+        @php 
+            $date = "";
+        @endphp
         @foreach($posts as $post)
-            <div class="card mt-3">
-                <div class="card-header">
-                    <span>{{$post->date}}</span>
+            @if($post->date != $date) 
+                @if(!$loop->first) {{-- daca nu e primul loop inchidem div --}}
                 </div>
-                <div class="card-body">
-                    <h3 class="card-title"><a href="/posts/{{$post->id}}">{{$post->category}}</a></h3>
-                    <p>{{$post->memo}}, {{$post->amount}}</p> 
-                    <small>de {{$post->user->name}}</small>              
-                </div>            
-            </div>
+                @endif
+                @php 
+                    $date = $post->date;
+                @endphp
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <span>{{$post->date}}</span>
+                    </div>
+                    <div class="card-body">
+                        <h3 class="card-title"><a href="/posts/{{$post->id}}">{{$post->category}}</a></h3>
+                        <p>{{$post->memo}}, {{$post->amount}}</p> 
+                        <small>de {{$post->user->name}}</small>              
+                    </div>
+            @else 
+                    <hr>
+                    <div class="card-body">
+                        <h3 class="card-title"><a href="/posts/{{$post->id}}">{{$post->category}}</a></h3>
+                        <p>{{$post->memo}}, {{$post->amount}}</p> 
+                        <small>de {{$post->user->name}}</small>              
+                    </div>
+            @endif
+            @if($loop->last) {{-- daca e ultimul loop inchidem div --}}
+                </div>
+            @endif
         @endforeach
         {{-- paginate 
         {{$posts->links()}} --}}
