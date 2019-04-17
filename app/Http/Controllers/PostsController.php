e0bd8d4c77b65248645099e00fe0dc3617612d82
Giorgian Robescu <robescu.george96@gmail.com>
2019-04-17 21:35:44 +0300

    Cod mai elegant in posts index si cheltuieli si venituri totale pe fiecare card

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use DateTime;
 
 class PostsController extends Controller
 {
@@ -25,7 +26,53 @@ class PostsController extends Controller
     public function index()
     {
         $posts = Post::orderBy('date', 'desc')->get();
-        return view('posts.index')->with('posts', $posts);
+
+        // Obtine lunile in care avem postari
+        $months = [];
+        foreach ($posts as $post) {
+            $date_obj = new DateTime($post->date);
+            if(!in_array($date_obj->format('F Y'), $months)){
+                array_push($months, $date_obj->format('F Y'));
+            }
+        }
+
+        // Calculeaza venituri si cheltuieli pentru fiecare zi in parte
+        $incomes = [];
+        $expenses = [];
+
+        foreach ($posts as $post) {
+            if($post->type == 'Income'){
+                if(array_key_exists($post->date, $incomes)){
+                    $incomes[$post->date] += $post->amount;
+                }else{
+                    $incomes[$post->date] = $post->amount;
+                }
+            }else if($post->type == 'Expenses'){
+                if(array_key_exists($post->date, $expenses)){
+                    $expenses[$post->date] += $post->amount;
+                }else{
+                    $expenses[$post->date] = $post->amount;
+                }
+            }
+        }
+
+        // Grupeaza posturile in functie de zi
+        $days = [];
+        foreach ($posts as $post) {
+            if(array_key_exists($post->date, $days)){
+                array_push($days[$post->date], $post);
+            }else{
+                $days[$post->date] = [];
+                array_push($days[$post->date], $post);
+            }
+        }
+
+        return view('posts.index')->with([
+            'days' => $days,
+            'months' => $months,
+            'incomes' => $incomes,
+            'expenses' => $expenses
+        ]);
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,59 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    {{-- Get all the months in which we have posts --}}
-    @php
-        $months = [];
-        foreach ($posts as $post) {
-            $date_obj = new DateTime($post->date);
-            if(!in_array($date_obj->format('F Y'), $months)){
-                array_push($months, $date_obj->format('F Y'));
-            }
-        }
-    @endphp
-    <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Selectați luna
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            @foreach ($months as $month)
-                <a href="#" class="dropdown-item">{{$month}}</a>
-            @endforeach
+    @if(count($days) > 0)
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Selectați luna
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                @foreach ($months as $month)
+                    <a href="#" class="dropdown-item">{{$month}}</a>
+                @endforeach
+            </div>
         </div>
-    </div>
-    @if(count($posts) > 0)
-        @php 
-            $date = "";
-        @endphp
-        @foreach($posts as $post)
-            @if($post->date != $date) 
-                @if(!$loop->first) {{-- daca nu e primul loop inchidem div --}}
+        @foreach($days as $day => $posts)
+            <div class="card mt-3">
+                <div class="card-header">
+                    <span>{{$day}}</span>
+                    <span class="float-right">
+                        @if(array_key_exists($day, $incomes) && array_key_exists($day, $expenses))
+                            Incomes: {{$incomes[$day]}} | Expenses: {{$expenses[$day]}}
+                        @elseif(array_key_exists($day, $incomes))
+                            Incomes: {{$incomes[$day]}}
+                        @elseif(array_key_exists($day, $expenses))
+                            Expenses: {{$expenses[$day]}}
+                        @endif
+                    </span>
                 </div>
-                @endif
-                @php 
-                    $date = $post->date;
-                @endphp
-                <div class="card mt-3">
-                    <div class="card-header">
-                        <span>{{$post->date}}</span>
-                    </div>
+                @foreach ($posts as $post)
                     <div class="card-body">
                         <h3 class="card-title"><a href="/posts/{{$post->id}}">{{$post->category}}</a></h3>
                         <p>{{$post->memo}}, {{$post->amount}}</p> 
                         <small>de {{$post->user->name}}</small>              
                     </div>
-            @else 
-                    <hr>
-                    <div class="card-body">
-                        <h3 class="card-title"><a href="/posts/{{$post->id}}">{{$post->category}}</a></h3>
-                        <p>{{$post->memo}}, {{$post->amount}}</p> 
-                        <small>de {{$post->user->name}}</small>              
-                    </div>
-            @endif
-            @if($loop->last) {{-- daca e ultimul loop inchidem div --}}
-                </div>
-                <br>
-            @endif
+                    @if(!$loop->last)
+                        <hr>
+                    @endif                   
+                @endforeach
+            </div>
+            <br>
         @endforeach
         {{-- paginate 
         {{$posts->links()}} --}}
